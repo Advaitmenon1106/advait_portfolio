8208eefdb923983e6a5e81cccb0fef9bafee1db5
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-12 21:54:51 +0530

    Make a new component for a ribbon at the top of the page, and style it.

---
+++ src/Ribbon/ribbon.jsx
@@ -0,0 +1,15 @@
+import './ribbon.css'
+
+export function Ribbon() {
+    return (
+        <div id="ribbon">
+            <div id='ribbon-all-options'>
+                <div className="ribbon-option">Home</div>
+                <div className="ribbon-option">Skills</div>
+                <div className="ribbon-option">Education</div>
+                <div className="ribbon-option">Projects and Internships</div>
+                <div className="ribbon-option">Contact Me</div>
+            </div>
+        </div>
+    )
+}
