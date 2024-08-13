543dc616ae01b08b360b2a3345034c8324141b4c
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-13 16:51:15 +0530

    Add the Animated cursor, ribbon and landing page components to the main code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { MainPage } from './MainPage/MainPage'
+import AnimatedCursor from 'react-animated-cursor'
+import { Ribbon } from './Ribbon/ribbon'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AnimatedCursor color='98, 0, 117' outerSize={11} innerSize={8}/>
+    <Ribbon />
+    <MainPage />
+  </React.StrictMode>
 )
