ae6ef74092fe03e49095f646794473ff58dace60
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-28 15:21:58 +0530

    Add the router Link to each menu option.

--- src/Ribbon/ribbon.jsx
+++ src/Ribbon/ribbon.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom'
 import './ribbon.css'
 
 export function Ribbon() {
     return (
         <div id="ribbon">
             <div id='ribbon-all-options'>
-                <div className="ribbon-option">Home</div>
-                <div className="ribbon-option">Skills</div>
+                <Link to={'/'}><div className="ribbon-option">Home</div></ Link>
+                <Link to={'skills'}><div className="ribbon-option">Skills</div></ Link>
                 <div className="ribbon-option">Education</div>
                 <div className="ribbon-option">Projects and Internships</div>
                 <div className="ribbon-option">Contact Me</div>
