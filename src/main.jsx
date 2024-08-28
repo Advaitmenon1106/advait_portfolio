fa270e4a1ef2d6a641a3ec8373079d3dfb8fe7c4
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-28 15:20:08 +0530

    Modify the code to accomodate React Routing, given that it is a multi-page application.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { MainPage } from './MainPage/MainPage'
 import AnimatedCursor from 'react-animated-cursor'
-import { Ribbon } from './Ribbon/ribbon'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { SkillsPage } from './SkillsPage/SkillsPage'
+
+const router = createBrowserRouter([
+
+  {
+    path:'/',
+    element: <MainPage />,
+  },
+
+  {
+    path: 'skills',
+    element: <SkillsPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AnimatedCursor color='98, 0, 117' outerSize={11} innerSize={8}/>
-    <Ribbon />
-    <MainPage />
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>
 )
